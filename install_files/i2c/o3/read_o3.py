978695d907657b957f44b3ed050e3a7b1b9062a1
saoded <saoded@gmail.com>
2026-03-17 19:43:13 +0200

    Improve comments and change measurement mode to passive
    Updated comments for clarity on collection number and IIC mode. Changed measurement mode from automatic to passive for less frequent measurements.

--- install_files/i2c/o3/read_o3.py
+++ install_files/i2c/o3/read_o3.py
@@ -4,13 +4,15 @@ import time
 import json
 from DFRobot_Ozone import *
 
-COLLECT_NUMBER   = 20              # collect number, the collection range is 1-100
-IIC_MODE         = 0x01            # default use IIC1
+COLLECT_NUMBER   = 20              # how many samples should the sensor collect & average: range is 1-100, increase if too spiky, decrease if too slow. 20-50 is recommended
+IIC_MODE         = 0x01            # default use IIC1, IIc0 is older and irrelevant
 
 '''
    The first  parameter is to select i2c0 or i2c1
-   The second parameter is the i2c device address
+   The second parameter is the i2c device address,
+   as determined by the switched on the back
    The default address for i2c is OZONE_ADDRESS_3
+   verify by running `i2cdetect -y 1`
       OZONE_ADDRESS_0        0x70
       OZONE_ADDRESS_1        0x71
       OZONE_ADDRESS_2        0x72
@@ -22,7 +24,7 @@ ozone = DFRobot_Ozone_IIC(IIC_MODE ,OZONE_ADDRESS_3)
     MEASURE_MODE_AUTOMATIC  active  mode
     MEASURE_MODE_PASSIVE    passive mode
 ''' 
-ozone.set_mode(MEASURE_MODE_AUTOMATIC)
+ozone.set_mode(MEASURE_MODE_PASSIVE) #stick to passive for a system that measures only every few minutes
 
 ''' Smooth data collection the collection range is 1-100 '''
 def get_single_read():
